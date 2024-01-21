e69758b0a87780c91ff5b6456dc8dcbb2678ce82
Vishal kumar <vishalsinghsaini1720@gmail.com>
2024-01-21 23:11:04 +0530

    fix colors

--- src/Components/Video-Component.js
+++ src/Components/Video-Component.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const VideoComponent = () => (
-  <div className="h-screen bg-lime-300 flex items-center justify-center">
+  <div className="h-screen bg-gray-200 flex items-center justify-center">
     <h1>Video comes here</h1>
   </div>
 );
